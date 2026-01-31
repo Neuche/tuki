9337d63be12ccf4ac73a755574200bd9ed2d315b
neuche <igna.eg@gmail.com>
2026-01-31 18:31:17 -0300

    Final cleanup of area_2d.gd to remove unused logic and silence debug prints

--- scenes/zona_trabajo/area_2d.gd
+++ scenes/zona_trabajo/area_2d.gd
@@ -1,21 +1,9 @@
 extends Area2D
-# Called when the node enters the scene tree for the first tim
-var current_drageable 
-var new_texture 
+
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _on_area_entered(drageable: Area2D) -> void:
-	if drageable.is_in_group("dropable"):
-		current_drageable = drageable
-		print("te vi")
-	pass # Replace with function body.
-
-func _input(event: InputEvent) -> void:
+func _on_area_entered(_drageable: Area2D) -> void:
 	pass
 
 		
