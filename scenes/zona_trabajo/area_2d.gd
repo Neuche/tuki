eb6d4086cbf004336a4e25200ee02c116df0b8cd
neuche <igna.eg@gmail.com>
2026-01-31 18:25:26 -0300

    Prevent item duplication by disabling legacy Area2D drop logic in favor of DropZone

--- scenes/zona_trabajo/area_2d.gd
+++ scenes/zona_trabajo/area_2d.gd
@@ -16,21 +16,6 @@ func _on_area_entered(drageable: Area2D) -> void:
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	if(event.is_action_released("realese_left_click")):
-		if current_drageable == null: return
-		var new_texture = TextureRect.new()
-		
-		self.add_child(new_texture,false) 
-		#tamaños
-		new_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-		new_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-		var tamano_especifico = Vector2(100, 100)
-		#fin de tamaño
-		new_texture.texture = current_drageable.get_parent()._get_texture()
-		var mouse_pos = get_global_mouse_position()
-		new_texture.global_position = mouse_pos - (new_texture.size / 2)
-		new_texture.custom_minimum_size = tamano_especifico
-		new_texture.size = tamano_especifico
-		
-		current_drageable = null
+	pass
+
 		
